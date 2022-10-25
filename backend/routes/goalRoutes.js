f31e697dabe78357c370f2ac357d7c748fa985cd
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-10-26 00:15:40 +0530

    feat: JWT Authentication

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const {getGoals, setGoal, updateGoal, deleteGoal} = require('../goalController/goalController');
+const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controller/goalController');
+
+const {protect} = require('../middleware/authMiddleware');
+
+router.route('/').get(protect, getGoals).post(protect, setGoal);
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal);
 
 module.exports = router;
-
-router.route('/').get(getGoals).post(setGoal);
-router.route('/:id').delete(deleteGoal).put(updateGoal);
-
-module.eports = router;
