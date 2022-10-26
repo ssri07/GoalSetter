d6a067250f5618370619a56f11d942b003612f66
Kabir <44284877+kabir0x23@users.noreply.github.com>
2022-10-26 22:47:46 +0530

    feat: redux goals and deploy (#9)

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controller/goalController');
+const {getGoals, setGoal, updateGoal, deleteGoal} = require('../controllers/goalController');
 
 const {protect} = require('../middleware/authMiddleware');
 
