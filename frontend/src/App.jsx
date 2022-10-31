f0d70fdad39075d8448e66702cbc67a5e925d2bd
Sanskar Srivastava <59871104+sanskarsri@users.noreply.github.com>
2022-10-31 23:01:39 +0530

    fix: fixed header search bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route}, {useState} from 'react-router-dom';
+import {useDispatch} from "react-redux";
+import {createGoal} from "../features/goals/goalSlice";
 
 import Dashboard from "./pages/Dashboard.jsx";
 import Register from "./pages/Register.jsx";
 import Login from "./pages/Login.jsx";
 
 const App = () => {
+    
+    const [text, setText] = useState('');
+
+    const dispatch = useDispatch();
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        dispatch(createGoal({text}));
+        setText('');
+    }
+    
     return (<>
         <div className="container">
 
